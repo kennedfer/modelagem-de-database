6276d3828de45956ed3f648234599f0c29e679f6
kennedfer <kenned14santos@gmail.com>
2024-04-26 19:41:15 -0300

    feat: tableas sao editaveis mas ainda nao apagaveis

--- interpreter.js
+++ interpreter.js
@@ -1,7 +1,16 @@
-class DiagramElement {}
+class DiagramElement{
+  
+}
 
-class TableElement extends DiagramElement {
+class TableElement extends DiagramElement{
+  element;
+
   constructor(tableStr) {
+    this.parse(tableStr);
+    super();
+  }
+
+  parse(tableStr) {
     const tableSplit = tableStr.split("\n");
 
     this.title = tableSplit.shift().split(" ")[1].replace("{", "");
@@ -19,13 +28,30 @@ class TableElement extends DiagramElement {
     }
   }
 
-  show() {
+  getIdentifier() {
+    return this.title;
+  }
+
+  create() {
     const tableElement = document.createElement("div");
+    tableElement.id = this.title + "-table";
     tableElement.className = "table";
 
+    tableElement.draggable = true;
+    tableElement.addEventListener("dragend", e => {
+      console.log(e);
+      tableElement.style.left = e.x+"px";
+      tableElement.style.top = e.y+"px";
+    });
+
     const titleElement = document.createElement("span");
+    titleElement.id = this.title + "-title";
+
     titleElement.textContent = this.title;
     tableElement.appendChild(titleElement);
+
+    const attributesContainer = document.createElement("div");
+    attributesContainer.id = this.title + "-attributes";
 
     for (let atribute of this.attributes) {
       const atributeParentElement = document.createElement("div");
@@ -40,10 +66,55 @@ class TableElement extends DiagramElement {
       atributeParentElement.appendChild(atributeNameElement);
       atributeParentElement.appendChild(atributeTypeElement);
 
-      tableElement.appendChild(atributeParentElement);
+      attributesContainer.appendChild(atributeParentElement);
     }
 
+    tableElement.appendChild(attributesContainer);
+
+    this.element = tableElement;
     return tableElement;
+  }
+
+  edit() {
+    // const tableElement = document.getElementById(this.title + "-table");
+
+    const titleElement = document.getElementById(this.title + "-title");
+    titleElement.textContent = this.title;
+
+    const attributesContainer = document.getElementById(
+      this.title + "-attributes"
+    );
+
+    attributesContainer.innerHTML = "";
+
+    for (let atribute of this.attributes) {
+      const atributeParentElement = document.createElement("div");
+      atributeParentElement.className = "table__attributes";
+
+      const atributeNameElement = document.createElement("span");
+      const atributeTypeElement = document.createElement("span");
+
+      atributeNameElement.textContent = atribute.name;
+      atributeTypeElement.textContent = atribute.type;
+
+      atributeParentElement.appendChild(atributeNameElement);
+      atributeParentElement.appendChild(atributeTypeElement);
+
+      attributesContainer.appendChild(atributeParentElement);
+    }
+
+    console.log(attributesContainer);
+    return this.element;
+  }
+
+  show() {
+    console.log(this.element);
+
+    if (!this.element) {
+      return this.create();
+    }
+
+    return this.edit();
   }
 }
 
@@ -56,22 +127,25 @@ class TableElement extends DiagramElement {
 // }
 
 class Interpreter {
-  tableBlocks;
+  elements = {};
 
-  show() {
-    const parentElement = document.getElementById("res");
-    parentElement.innerHTML = "";
+  // show() {
+  //   const parentElement = document.getElementById("res");
+  //   // parentElement.innerHTML = ""; //APAGANDO TUDO DO PANEL
+  //   const elements = Object.values(this.elements);
 
-    for (let table of this.tableBlocks) {
-      // console.log(table);
-      parentElement.appendChild(table.show());
-    }
-  }
+  //   for (let element of elements) {
+  //     // console.log(table);
+  //     element.show();
+  //   }
+  // }
 
   interpret(code) {
     this.tableBlocks = [];
 
     let lastCloseIndex = 0;
+
+    const parentElement = document.getElementById("res");
 
     for (let i in code) {
       const letter = code[i];
@@ -82,11 +156,23 @@ class Interpreter {
         const tableStr = code.substring(lastCloseIndex, index).trim();
         lastCloseIndex = index;
 
-        this.tableBlocks.push(new TableElement(tableStr));
+        //ISSO PODE SER LENTO POIS CRIA VARIOS OBJETOS
+        //MUDAR DEPOIS PARA CRIAR UM NOVO APENAS SE FOR
+        //DE FATO UMA NOVA TABELA
+        const tableElement = new TableElement(tableStr);
+        const tableTitle = tableElement.getIdentifier();
+
+        if (this.elements[tableTitle]) {
+          this.elements[tableTitle].parse(tableStr);
+          this.elements[tableTitle].edit();
+        } else {
+          this.elements[tableTitle] = tableElement;
+          parentElement.appendChild(tableElement.show());
+        }
       }
     }
 
-    this.show();
+    // this.show();
   }
 }
 
