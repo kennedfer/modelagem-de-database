90e1d7cd62fbf8532aeb807ba73624b074031704
kennedfer <kenned14santos@gmail.com>
2024-04-30 08:33:04 -0300

    feat: Mudar o input de textarea para uma ol editavel

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,11 @@ import interpreter from "./interpreter/interpreter.js";
 
 const codeTextarea = document.getElementById("code-textarea");
 
-codeTextarea.onchange = function (e) {
-  const code = e.target.value;
+codeTextarea.onblur = function (e) {
+  // const code = e.target.value;
+  const code = Array.from(codeTextarea.children).reduce(
+    (accumulator, currentValue) => accumulator + currentValue.textContent + "\n",
+    "",);
 
   try {
     interpreter.interpret(code);
