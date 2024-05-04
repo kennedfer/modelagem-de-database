a8ec532890b2a349667d1c487954b23ab6ed13a5
kennedfer <kenned14santos@gmail.com>
2024-05-04 08:48:22 -0300

    fix: Juntar a logica do objeto de elementos no arquivo do DiagramController

--- scripts/elements/DiagramController.js
+++ scripts/elements/DiagramController.js
@@ -1,4 +1,10 @@
-import DIAGRAM_ELEMENTS_TAGS from "./elements-tags.js";
+import RelationshipElement from "./relationship/RelationshipElement.js";
+import TableElement from "./table/TableElement.js";
+
+const DIAGRAM_ELEMENTS_TAGS = {
+  tabela: TableElement,
+  relacao: RelationshipElement
+}
 
 class DiagramController {
   static createElement(tag, elementStr) {
--- scripts/elements/elements-tags.js
+++
@@ -1,9 +0,0 @@
-import RelationshipElement from "./relationship/RelationshipElement.js";
-import TableElement from "./table/TableElement.js";
-
-const DIAGRAM_ELEMENTS_TAGS = {
-  tabela: TableElement,
-  relacao: RelationshipElement
-}
-
-export default DIAGRAM_ELEMENTS_TAGS;
