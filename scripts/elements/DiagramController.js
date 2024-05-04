305e30cd28fb312ec4b6b0ead3a71590c0a3abd5
kennedfer <kenned14santos@gmail.com>
2024-05-04 08:46:50 -0300

    fix: Remover o construtor do DiagramController

--- scripts/elements/DiagramController.js
+++ scripts/elements/DiagramController.js
@@ -1,8 +1,6 @@
 import DIAGRAM_ELEMENTS_TAGS from "./elements-tags.js";
 
 class DiagramController {
-  constructor() { }
-
   static createElement(tag, elementStr) {
     return (new DIAGRAM_ELEMENTS_TAGS[tag](elementStr));
   }
