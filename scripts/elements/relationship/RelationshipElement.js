a2c6b49e9238e3ed7d37768f668436b0fed78bbb
kennedfer <kenned14santos@gmail.com>
2024-05-05 11:59:39 -0300

    fix: Refatorar e melhorar o codigo

--- scripts/elements/relationship/RelationshipElement.js
+++ scripts/elements/relationship/RelationshipElement.js
@@ -14,11 +14,33 @@ class RelationshipElement extends DiagramElement {
     this.parse(relationshipStr);
   }
 
+  #createLines() {
+    const line = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+
+    const svgContainer = document.getElementById("svg-container");
+    svgContainer.appendChild(line);
+
+    this.relationshipLine = line;
+  }
+
+  #applyDragEventToRelationTables() {
+    const firstRelationItem = document.getElementById(this.relationItens[0].trim() + "-table");
+    firstRelationItem.addEventListener("drag", () => this.calculateLines(this.relationItens));
+
+    const lastRelationItem = document.getElementById(this.relationItens[1].trim() + "-table");
+    lastRelationItem.addEventListener("drag", () => this.calculateLines(this.relationItens));
+  }
+
+  #updateCardinalitiesText() {
+    this.cardinalityLastToFirst.textContent = this.cardinalities[0];
+    this.cardinalityFirstToLast.textContent = this.cardinalities[1];
+  }
+
   parse(relationshipStr) {
     const relationshipSplit = relationshipStr.split("\n");
-    const relationshipTitle = relationshipSplit.shift().split(" ")[1];
+    const relationshipTitle = (relationshipSplit.shift()).split(" ")[1];
 
-    this.title = relationshipTitle.substring(0, relationshipTitle.length - 1);
+    this.title = relationshipTitle.slice(0, -1);
     relationshipSplit.pop();
 
     this.relationItens = relationshipSplit.shift().split("->");
@@ -26,43 +48,33 @@ class RelationshipElement extends DiagramElement {
   }
 
   calculateLines(relationItens) {
-    const firstRelationItem = document.getElementById(relationItens[0].trim() + "-table");
-    const lastRelationItem = document.getElementById(relationItens[1].trim() + "-table");
+    const firstRelationItem = document.getElementById(`${relationItens[0].trim()}-table`);
+    const lastRelationItem = document.getElementById(`${relationItens[1].trim()}-table`);
 
-    const firstRelationItemX = Number(firstRelationItem.style.left.replace("px", ""));
-    const firstRelationItemY = Number(firstRelationItem.style.top.replace("px", ""));
+    const firstRelationItemX = Number(firstRelationItem.style.left.slice(0, -2));
+    const firstRelationItemY = Number(firstRelationItem.style.top.slice(0, -2));
 
-    const lastRelationItemX = Number(lastRelationItem.style.left.replace("px", ""));
-    const lastRelationItemY = Number(lastRelationItem.style.top.replace("px", ""));
+    const lastRelationItemX = Number(lastRelationItem.style.left.slice(0, -2));
+    const lastRelationItemY = Number(lastRelationItem.style.top.slice(0, -2));
 
-    const relationItemX = Number(this.element.style.left.replace("px", ""));
-    const relationItemY = Number(this.element.style.top.replace("px", ""));
+    const relationItemX = Number(this.element.style.left.slice(0, -2));
+    const relationItemY = Number(this.element.style.top.slice(0, -2));
 
-    const line = this.relationshipLine;
-    line.setAttributeNS(null, "d", `M ${firstRelationItemX} ${firstRelationItemY} 
+    this.relationshipLine.setAttributeNS(null, "d", `M ${firstRelationItemX} ${firstRelationItemY} 
     L ${firstRelationItemX} ${relationItemY} L ${relationItemX} ${relationItemY} 
     L ${lastRelationItemX} ${relationItemY} L ${lastRelationItemX} ${lastRelationItemY}`);
 
-    this.cardinalityLastToFirst.textContent = this.cardinalities[0];
-    this.cardinalityFirstToLast.textContent = this.cardinalities[1];
+    this.#updateCardinalitiesText();
 
-    this.cardinalityLastToFirst.style.top = relationItemY + "px";
-    this.cardinalityLastToFirst.style.left = firstRelationItemX + "px";
+    this.cardinalityLastToFirst.style.top = `${relationItemY}px`;
+    this.cardinalityLastToFirst.style.left = `${firstRelationItemX}px`;
 
-    this.cardinalityFirstToLast.style.top = relationItemY + "px";
-    this.cardinalityFirstToLast.style.left = lastRelationItemX + "px";
-  }
-
-  createLines() {
-    const line = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-    const svgContainer = document.getElementById("svg-container");
-    svgContainer.appendChild(line);
-
-    this.relationshipLine = line;
+    this.cardinalityFirstToLast.style.top = `${relationItemY}px`;
+    this.cardinalityFirstToLast.style.left = `${lastRelationItemX}px`;
   }
 
   create() {
-    this.createLines();
+    this.#createLines();
 
     this.cardinalityLastToFirst = document.createElement("span");
     this.cardinalityFirstToLast = document.createElement("span");
@@ -86,13 +98,8 @@ class RelationshipElement extends DiagramElement {
     tableElement.appendChild(titleElement);
 
     this.element = tableElement;
-    console.log(this.relationItens);
 
-    const firstRelationItem = document.getElementById(this.relationItens[0].trim() + "-table");
-    firstRelationItem.addEventListener("drag", () => this.calculateLines(this.relationItens));
-
-    const lastRelationItem = document.getElementById(this.relationItens[1].trim() + "-table");
-    lastRelationItem.addEventListener("drag", () => this.calculateLines(this.relationItens));
+    this.#applyDragEventToRelationTables();
 
     this.element.addEventListener("drag", () => this.calculateLines(this.relationItens));
     this.onDragElement(this.element);
@@ -104,15 +111,10 @@ class RelationshipElement extends DiagramElement {
     const titleElement = document.getElementById(this.title + "-title");
     titleElement.textContent = this.title;
 
+    this.#applyDragEventToRelationTables();
+    this.#updateCardinalitiesText();
+
     return this.element;
-  }
-
-  show() {
-    if (!this.element) {
-      return this.create();
-    }
-
-    return this.edit();
   }
 }
 
